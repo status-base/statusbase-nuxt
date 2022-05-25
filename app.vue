35cff6fd9bc5b201f02a334c237e71fdbe89211c
zernonia <zernonia@gmail.com>
2022-05-25 20:50:36 +0800

    add banner

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+//@ts-ignore
+import Banner from "assets/banner.png"
+</script>
 <template>
-  <div class="p-4 md:p-8 w-full max-w-screen-xl">
-    <NuxtPage />
+  <div class="w-screen flex flex-col items-center">
+    <img class="w-full h-64 object-fill" :src="Banner" />
+    <div class="w-full max-w-screen-lg">
+      <NuxtPage />
+    </div>
   </div>
 </template>
