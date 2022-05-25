ee36d7fb82fb3f62fa1cbcf4362ec431d237671e
zernonia <zernonia@gmail.com>
2022-05-25 23:48:56 +0800

    add incident reports

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 const { data: urls } = await useAsyncData("urls", () => queryContent("/urls").find())
-const { data: reports } = await useAsyncData("reports", () => queryContent("/logs").find())
+const { data: logs } = await useAsyncData("logs", () => queryContent("/logs").find())
+const { data: incidents } = await useAsyncData("reports", () => queryContent("/incidents").sort({ title: 0 }).find())
+
+console.log({ incidents })
 </script>
 
 <template>
   <div>
     <h1 class="text-white text-4xl font-semibold mt-20 mb-10">StatusBase</h1>
-    <OverallStatus :report_data="reports"></OverallStatus>
+    <OverallStatus :report_data="logs"></OverallStatus>
 
     <div class="mt-20 font-semibold inline-flex items-end">
       <h2 class="text-3xl">Uptime</h2>
@@ -14,11 +17,20 @@ const { data: reports } = await useAsyncData("reports", () => queryContent("/log
     </div>
 
     <div class="flex flex-col items-center">
-      <Card :meta_data="url" :report_data="reports[index]" v-for="(url, index) in urls" :key="url._id"></Card>
+      <Card :meta_data="url" :report_data="logs[index]" v-for="(url, index) in urls" :key="url._id"></Card>
     </div>
 
     <div class="mt-20 font-semibold inline-flex items-end">
       <h2 class="text-3xl">Recent Incident</h2>
+    </div>
+
+    <div class="p-8 mt-6 rounded-xl bg-gradient-to-r from-cyan-50 to-purple-50">
+      <div class="mb-8" v-for="(incident, index) in incidents" :key="incident._id">
+        <h3 class="text-lg font-medium mb-4">{{ incident.date }}</h3>
+        <ContentRenderer class="prose-sm" :value="incident" />
+
+        <hr class="mt-6" />
+      </div>
     </div>
   </div>
 </template>
