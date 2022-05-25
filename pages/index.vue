32e4ede14922649c2b8eb3f71d8e19ad743d9eb6
zernonia <zernonia@gmail.com>
2022-05-25 20:21:18 +0800

    change ways to do health-check

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-const { data } = await useAsyncData("reports", () => queryContent("/log").find())
+const { data: urls } = await useAsyncData("urls", () => queryContent("/url").find())
+const { data: reports } = await useAsyncData("reports", () => queryContent("/log").find())
 </script>
 
 <template>
-  <div class="bg-white p-8">
-    <Card :data="report" v-for="report in data" :key="report._id"></Card>
+  <div>
+    <div class="flex flex-col items-center bg-white p-8">
+      <Card :meta_data="url" :report_data="reports[index]" v-for="(url, index) in urls" :key="url._id"></Card>
+    </div>
   </div>
 </template>
