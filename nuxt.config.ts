14f81a6ee9d493f06bf6bdf6ed5a6e34e3c4eb2f
zernonia <zernonia@gmail.com>
2022-05-25 20:21:47 +0800

    add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ import { defineNuxtConfig } from "nuxt"
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/main.css"],
-  modules: ["nuxt-windicss", "@nuxt/content"],
+  modules: ["nuxt-windicss", "@nuxt/content", "unplugin-icons/nuxt"],
 })
