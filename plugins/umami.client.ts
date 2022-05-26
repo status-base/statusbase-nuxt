b0516f8dacdb6847d85de6be473b8d88d2bc5cd2
zernonia <zernonia@gmail.com>
2022-05-26 21:21:24 +0800

    add umami

---
+++ plugins/umami.client.ts
@@ -0,0 +1,25 @@
+import { defineNuxtPlugin } from "#app"
+
+export default defineNuxtPlugin(() => {
+  const moduleOptions = {
+    scriptUrl: "https://umami-zernonia.vercel.app/umami.js",
+    websiteId: "9d53c455-d1a9-48b1-8c7d-145650db4f62",
+    domains: "statusbase.vercel.app",
+  }
+  const options = { ...moduleOptions }
+
+  loadScript(options)
+})
+
+function loadScript(options: any) {
+  const head = document.head || document.getElementsByTagName("head")[0]
+  const script = document.createElement("script")
+
+  script.async = true
+  script.defer = true
+  script.dataset.websiteId = options.websiteId
+  script.dataset.domains = options.domains
+  script.src = options.scriptUrl
+
+  head.appendChild(script)
+}
