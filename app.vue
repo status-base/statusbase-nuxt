90f6846976211e24f2243d2534d6a3bef74349cc
zernonia <zernonia@gmail.com>
2022-06-17 00:38:48 +0800

    update text to nuxt

--- app.vue
+++ app.vue
@@ -17,7 +17,7 @@ watch(windowWidth, (n) => (gridCount.value = n > 768 ? 45 : n > 400 ? 30 : 20))
           to="/"
           class="text-white text-3xl md:text-4xl font-semibold inline-flex items-center text-shadow-md shadow-purple-500"
         >
-          <Logo class="mr-4"></Logo> StatusBase
+          <Logo class="mr-4"></Logo> StatusBase Nuxt
         </NuxtLink>
         <GithubButton class="mt-4 sm:mt-0"></GithubButton>
       </div>
--- components/GithubButton.vue
+++ components/GithubButton.vue
@@ -2,10 +2,13 @@
 import IconGitHub from "~icons/carbon/logo-github"
 import IconUp from "~icons/carbon/play-filled-alt"
 
-const { data } = await useLazyFetch<{ stargazers_count: number }>("https://api.github.com/repos/zernonia/statusbase", {
-  pick: ["stargazers_count"],
-  server: false,
-})
+const { data } = await useLazyFetch<{ stargazers_count: number }>(
+  "https://api.github.com/repos/status-base/statusbase-nuxt",
+  {
+    pick: ["stargazers_count"],
+    server: false,
+  }
+)
 </script>
 
 <template>
@@ -13,9 +16,9 @@ const { data } = await useLazyFetch<{ stargazers_count: number }>("https://api.g
     rel="noopener"
     class="bg-white rounded-lg p-2 shadow-lg shadow-purple-100 text-gray-400 hover:text-gray-700 transition flex items-center"
     style="font-family: Arial, Helvetica, sans-serif; height: 54px"
-    href="https://github.com/zernonia/statusbase"
+    href="https://github.com/status-base/statusbase-nuxt"
     target="_blank"
-    aria-label="Star zernonia/statusbase on GitHub"
+    aria-label="Star status-base/statusbase-nuxt on GitHub"
   >
     <div class="mr-2 inline-flex">
       <IconGitHub class="w-7 h-7 md:w-9 md:h-9"></IconGitHub>
